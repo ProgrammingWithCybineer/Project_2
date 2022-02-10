f197354331380d72c5c00d13b9aa38e55c914ae6
Simz014 <asimaha123@gmail.com>
2022-02-10 15:22:04 -0600

    query methods are created

--- src/main/scala/Project_2.scala
+++ src/main/scala/Project_2.scala
@@ -20,47 +20,101 @@ object Project_2 {
 
    //Logger.getLogger("org").setLevel(level.Error)
        
-                  val spark=SparkSession
+//Loading CSV file and creating DataFrame
+ def covidData():Unit={
+                   val spark=SparkSession
                   .builder
                   .appName("sparkSQL")
                   .master("local[*]")
-                   .getOrCreate()
+                  .getOrCreate()
                   
-                   val csvFile =spark.read.format("csv")
+                    val csvFile =spark.read.format("csv")
                     .option("mode", "FAILFAST")
                     .option("inferSchema", "true")
                     .option("header", "true")
                     .load("input/covid-data.csv")
-                      
-                     /*
-                    // Creates Create Covid Data Database
-                    def covidData(){
-                        val output = hiveCtx.read
-                            .format("csv")
-                            .option("inferSchema", "true")
-                            .option("header", "true")
-                            .load("input/covid-data.csv")
-                              */      
-                        //output.limit(20).show() // will print out the first 20 lines
-                        csvFile.printSchema()
-
-                        csvFile.createOrReplaceTempView("temp_data")
+                     csvFile.printSchema()
+                    csvFile.createOrReplaceTempView("temp_data")
                      
                         
-                        
+ }                   
                     
 
                     
 
                     // Query for total number of shark attacks since certain date
-                  
+                def method1():Unit={
                         println("Title of Query")
                         val result = spark.sql("select  (max(total_deaths)-min(total_deaths)) as DifferenceInDeath from temp_data")
                         result.show(160)
                         Thread.sleep(100000)
-                       result.write.mode("overwrite").csv("results/")
+                        result.write.mode("overwrite").csv("results/")
                         
-                        //log.write("")
+                }
+             def method2(){
+                      println("Title of Query")
+                        val result = spark.sql("")
+                        result.show(160)
+                        Thread.sleep(100000)
+                        result.write.mode("overwrite").csv("results/")
+                    }
+            def method3():Unit={
+                        println("Title of Query")
+                        val result = spark.sql("")
+                        result.show(160)
+                        Thread.sleep(100000)
+                        result.write.mode("overwrite").csv("results/")
+                    }
+             def method4():Unit={
+                        println("Title of Query")
+                        val result = spark.sql("")
+                        result.show(160)
+                        Thread.sleep(100000)
+                        result.write.mode("overwrite").csv("results/")
+                    }  
+             def method5():Unit={
+                        println("Title of Query")
+                        val result = spark.sql("")
+                        result.show(160)
+                        Thread.sleep(100000)
+                        result.write.mode("overwrite").csv("results/")
+                    } 
+             def method6():Unit={
+                        println("Title of Query")
+                        val result = spark.sql("")
+                        result.show(160)
+                        Thread.sleep(100000)
+                        result.write.mode("overwrite").csv("results/")
+                    } 
+             def method7():Unit={
+                        println("Title of Query")
+                        val result = spark.sql("")
+                        result.show(160)
+                        Thread.sleep(100000)
+                        result.write.mode("overwrite").csv("results/")
+                    } 
+             def method8():Unit={
+                        println("Title of Query")
+                        val result = spark.sql("")
+                        result.show(160)
+                        Thread.sleep(100000)
+                        result.write.mode("overwrite").csv("results/")
+                    } 
+             def method9():Unit={
+                        println("Title of Query")
+                        val result = spark.sql(" ")
+                        result.show(160)
+                        Thread.sleep(100000)
+                        result.write.mode("overwrite").csv("results/")
+                    } 
+             def method10():Unit={
+                        println("Title of Query")
+                        val result = spark.sql("")
+                        result.show(160)
+                        Thread.sleep(100000)
+                        result.write.mode("overwrite").csv("results/")
+                    } 
+
 
                         spark.stop()
                     }
